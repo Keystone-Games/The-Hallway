150ec383264df7a13992e9af2e97fd4337c0c9d4
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-10-05 20:30:19 -0400

    Update MenuState.hx

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -1,15 +1,23 @@
 package states;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 
 class MenuState extends FlxState
 {
 	var title:FlxText;
+	var background:FlxSprite;
+	var playButton:FlxSprite;
+	var optionsButton:FlxText;
 
 	public override function create()
 	{
 		super.create();
+
+		title = new FlxText(0, 0, 0, "The Hallway");
+		title.screenCenter(X);
+		add(title);
 	}
 
 	public override function update(dt:Float)
