d23da30312819babbcebb6e47cd93805dc8e23cf
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-10-06 09:22:56 -0400

    Update Options.hx

--- src/config/Options.hx
+++ src/config/Options.hx
@@ -3,5 +3,8 @@ package config;
 class Options {
   public static var VOLUME_MUSIC = 1;
   public static var VOLUME_SFX = 1;
-  
+ 
+  public static var VFX_INTENSITY = 1;
+  public static var VFX_QUALITY = 1;
+  public static var GRAPHICS_QUALITY = 1;
 }
