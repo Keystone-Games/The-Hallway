e78ae8dd89245ac0d2a6da92d992baa453c25529
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-10-06 06:58:10 -0400

    Update Reference.hx

--- src/config/Reference.hx
+++ src/config/Reference.hx
@@ -2,7 +2,9 @@ package config;
 
 class Reference
 {
-	public static var app_name = "The Hallway";
-	public static var app_id = "p5_the_hallway";
-	public static var app_version = "0.0.1";
+	public static final APP_NAME = "The Hallway";
+	public static final APP_ID = "p5_the_hallway";
+	public static final APP_VERSION = "0.0.1";
+	
+	public static final OPTIONS_FILE_PATH = Path.join([System.ApplicationStorageDirectory, "config.json"]);
 }
