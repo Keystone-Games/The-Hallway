6c4a44c119c838971e3ced0de5cfdede45458b66
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-10-05 20:18:15 -0400

    Update MenuState.hx

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -1,9 +1,12 @@
 package states;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class MenuState extends FlxState
 {
+	var title:FlxText;
+
 	public override function create()
 	{
 		super.create();
