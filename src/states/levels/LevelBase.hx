03f3a2c827f2b15db5d5fde343947b421cebed14
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-10-08 14:37:14 -0400

    Update LevelBase.hx

--- src/states/levels/LevelBase.hx
+++ src/states/levels/LevelBase.hx
@@ -3,8 +3,21 @@ package states.levels;
 class LevelBase extends FlxState {
   final level = 0;
   
-  var title:FlxSprite;
+  var baseMap:{};
+  var title:FlxText;
   
-  public override function create() { super.create() }
-  public override function update( dt:Float ) { super.update( dt ) }
+  public override function create() { 
+    super.create();
+    
+    initMap();
+    title = new FlxText(0, 0, 0, "Level " + level).setFormat("monsterrat", 64);
+    add(title);
+  }
+  public override function update( dt:Float ) { super.update( dt ); }
+  private static function initMap() { 
+    baseMap = {
+      floor: {},
+      rooms: {}
+    };
+  }
 }
