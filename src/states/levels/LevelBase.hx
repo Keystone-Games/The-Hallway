044bb496aafaa7036f33cc2db3a2af13932564d2
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-10-08 10:05:32 -0400

    Create LevelBase.hx

---
+++ src/states/levels/LevelBase.hx
@@ -0,0 +1,10 @@
+package states.levels;
+
+class LevelBase extends FlxState {
+  final level = 0;
+  
+  var title:FlxSprite;
+  
+  public override function create() { super.create() }
+  public override function update( dt:Float ) { super.update( dt ) }
+}
