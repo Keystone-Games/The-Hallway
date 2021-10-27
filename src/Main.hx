c6e64987f9c54d68bae36222e17873f0bf7f2e09
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-10-27 10:06:57 -0400

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -30,5 +30,32 @@ class Main extends Sprite
 		SoundManager.playMusic(MENU_THEME);
 	}
 	
-	public static function onExit()
+	public static function onExit() {
+		// save data
+		#if sys
+		var directory = System.applicationStorageDirectory;
+		var oFile = "./data.json";
+		var oFPath = Path.join([directory, oFile]);
+		trace("Saving options data");
+		var data = {
+			"options":{
+				"fullscreen": FlxG.fullscreen,
+				"debugMode": false,
+				"touchscreenmode": false,
+				"pauseOnLostFocus": FlxG.autoPause
+			},
+			
+			"data":{
+				"playedTutorial": Reference.HAS_PLAYED_TUTORIAL,
+				"currentLevel": Reference.CURRENT_LEVEL,
+				"deaths": Reference.STAT_DEATHS,
+				"kills": Reference.STAT_KILLS
+			}
+		};
+		var content = Json.stringify(options);
+		File.saveContent(oFPath, data);
+
+		trace("Finished saving data!");
+		#end
+	}
 }
