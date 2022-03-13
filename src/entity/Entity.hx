55f42eed217b65a7b72e8172b937a44c234010db
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-03-12 20:37:56 -0500

    Update Entity.hx

--- src/entity/Entity.hx
+++ src/entity/Entity.hx
@@ -7,9 +7,29 @@ class Entity
 	public var baseSprite:FlxSprite;
 
 	public static function update(dt:Float) {}
+
+	public static function hit(tool:Tool)
+	{
+		switch (tool)
+		{
+			case PENCIL:
+				hpc();
+			case PEN:
+				hpn();
+			case STICK:
+				hst();
+		}
+	}
+
+	private static function hpc() {}
+
+	private static function hpn() {}
+
+	private static function hst() {}
 }
 
-extern enum Person {
+extern enum Person
+{
 	teacher;
 	student;
 }
